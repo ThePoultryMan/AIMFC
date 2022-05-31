3225f1f3f6131dc96a294149bec9ab3f9807f136
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-05-31 14:11:48 -0400

    Setup main class

--- src/main/java/io/github/thepoultryman/aimfc/ArmorCombat.java
+++ src/main/java/io/github/thepoultryman/aimfc/ArmorCombat.java
@@ -6,13 +6,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ArmorCombat implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod name as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("Example Mod");
+	public static final Logger LOGGER = LoggerFactory.getLogger("aimfc");
 
 	@Override
 	public void onInitialize(ModContainer mod) {
-		LOGGER.info("Hello Quilt world from {}!", mod.metadata().name());
+		LOGGER.info("AIMFC, Hiding your armor since 2022");
 	}
 }
