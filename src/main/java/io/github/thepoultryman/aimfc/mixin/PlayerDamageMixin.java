e95b1ee44a60fa3a88193a9c8829577b43f49ae5
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-05-31 23:07:42 -0400

    Use client player to retain client side

--- src/main/java/io/github/thepoultryman/aimfc/mixin/PlayerDamageMixin.java
+++ src/main/java/io/github/thepoultryman/aimfc/mixin/PlayerDamageMixin.java
@@ -1,14 +1,14 @@
 package io.github.thepoultryman.aimfc.mixin;
 
 import io.github.thepoultryman.aimfc.ArmorHidingHelper;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(PlayerEntity.class)
+@Mixin(ClientPlayerEntity.class)
 public class PlayerDamageMixin {
 	@Inject(method = "damage", at = @At("HEAD"))
 	private void aimfc$unhideArmor(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
